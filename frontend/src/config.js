25ca04038f3129b0286158c71185da48ed2e8176
Wong Yong Xi <115607618+ixgnoy@users.noreply.github.com>
2025-06-15 13:47:07 +0800

    Create config.js

---
+++ frontend/src/config.js
@@ -0,0 +1,8 @@
+const config = {
+  contractAddress: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
+  network: "localhost",
+  PINATA_JWT: "",
+};
+
+export default config;
+export const PINATA_JWT = config.PINATA_JWT;
